579e569e6bbd6b9c16b7fd5c881d399c8c37df32
Jonathan White <support@dmapps.us>
2019-03-29 10:55:40 -0400

    added more functions to globoards

--- gloBoards.py
+++ gloBoards.py
@@ -6,17 +6,22 @@ glo_api = 'https://gloapi.gitkraken.com/v1/glo'
 # Class GloBoardsApi init instance creates session variable token
 class GloBoardsApi:
     def __init__(self, glo_token):
-        self.payload = {'access_token': glo_token}
+        self.glo_token = {'access_token': glo_token}
 
     def get_boards(self):
-        r = requests.get(glo_api + '/boards', params = self.payload)
+        r = requests.get(glo_api + '/boards', params=self.glo_token)
         return r.json()
     
-    def get_columns(self):
-        pass
+    def get_columns(self, board_id):
+        payload = self.glo_token
+        payload["fields"] = "columns"
+        r = requests.get(glo_api + '/boards/{}'.format(board_id), params=payload)
+        return r.json()
 
-    def get_cards(self):
-        pass
+    def get_cards(self, board_id):
+        payload = self.glo_token
+        r = requests.get(glo_api + '/boards/{}/cards'.format(board_id), params=payload)
+        return r.json()
 
     def get_labels(self):
         pass
@@ -29,6 +34,13 @@ class GloBoardsApi:
     
     def get_userInfo(self):
         pass
+    
+    def move_card(self, board_id, card_id, new_column_id):
+        payload = {"column_id" : new_column_id}
+        requests.post(glo_api + '/boards/{}/cards/{}'.format(board_id, card_id), json=payload, params=self.glo_token)
+
+    def create_card(self, board_id, card_data):
+        requests.post(glo_api + '/boards/{}/cards'.format(board_id), json=card_data, params=self.glo_token)
 
 # Performs a full one-way transfer from zenhub to glo
 # Does not link back to GitHub
